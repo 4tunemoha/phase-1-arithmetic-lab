58f5a4420d784929aa61e3d6122c3906fa83405f
Fortune Mohammed <fortunemoha14@gmail.com>
2025-03-16 23:46:33 +0300

    limited

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+//Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+//Create a variable called random that will generate a random integer greater than 0.
+//Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+//Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+
+//let multiply, num1, num2, random, mod, max;
+//function times(num1, num2){
+//    return num1 * num2;
+//}
+//multiply = times(31, 2);
+//console.log(times(31, 2));
+//console.log(multiply);
+
+//#task1
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+//#task2
+let random = Math.floor(Math.random() * 100) + 1;
+//#task3
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+//#task4
+let max = Math.max(11, 17, 8, 20);
+//#mi finish
+
